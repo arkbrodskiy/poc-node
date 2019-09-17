eb82d6bbc0be0a75b482bc3b9fb48e9f0edcf076
ark <arkadiy.brodskiy@somoglobal.com>
2019-09-16 21:21:28 -0400

    EJS installed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Weren't you waiting for me???");
+// Setup
+
+var express = require('express'),
+    app = express()
+
+
+app.get("/", function(req, res){
+    res.render("home.ejs")
+});
+
+
+app.listen(3000, function(){
+    console.log("Serving app on port 3000");
+});
