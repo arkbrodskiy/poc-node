7e163a1a170a9cd10cc2a116428da6d38e0d1759
ark <arkadiy.brodskiy@somoglobal.com>
2019-09-17 21:40:50 -0400

    ejs and partials installed

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // Setup
 
-var express = require('express'),
-    app = express()
+var express = require('express');
+    app = express();
+
+app.use(express.static("assets"));
+app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res){
-    res.render("home.ejs")
+    res.render("home")
 });
 
 
